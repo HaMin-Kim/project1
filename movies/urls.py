67d409e661f56df595a70416797e6c58def073d3
최대환 <daehwan@choedaehwan-ui-MacBookPro.local>
2021-05-17 11:44:45 +0900

    FIX: 메인페이지 기능별로 엔드 포인트 다르게함

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls  import path
 
-from movies.views import MainPageView
+from movies.views import MovieRankingView, MovieRandomView
 
 urlpatterns= [
-	path('/movies', MainPageView.as_view()),
+	path('/ranking', MovieRankingView.as_view()), 
+	path('/random', MovieRandomView.as_view()),
 ]
--- movies/views.py
+++ movies/views.py
@@ -7,13 +7,18 @@ from django.db.models import Max
 from users.utils      import login_confirm
 from movies.models    import Movie, Comment
 
-class MainPageView(View):
+class MovieRankingView(View):
 	def get(self, request):
 		RANKING_LIMIT = 25
 		movie_ranking_data = Movie.objects.values(
 			'id', 'korean_title', 'country', 'release_date', 'thumbnail_img',
 		).order_by('-audience_count')[:RANKING_LIMIT]
+		
+		return JsonResponse({'박스오피스 순위': list(movie_ranking_data)}, status = 200)
 
+
+class MovieRandomView(View):
+	def get(self, request):
 		RANDOM_LIMIT = 10
 		netflix_random_data = Movie.objects.filter(netflix=True).values(
                         'id', 'korean_title', 'country', 'release_date', 'thumbnail_img',
@@ -24,7 +29,6 @@ class MainPageView(View):
                 ).order_by('?')[:RANDOM_LIMIT]
 
 		return JsonResponse({
-			'박스오피스 순위': list(movie_ranking_data),
 			'넷플릭스 추천': list(netflix_random_data), 
 			'왓챠 추천': list(watcha_random_data),
 		}, status = 200)
