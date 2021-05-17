2617f8c108907b5426d4098cdc7b8325a041dc68
최대환 <daehwan@choedaehwan-ui-MacBookPro.local>
2021-05-17 13:32:20 +0900

    in progress

--- movies/views.py
+++ movies/views.py
@@ -23,19 +23,18 @@ class MovieInformationView(View):
 		#			}
 		#		)
 			
-			LIMIT = 10
+#			LIMIT = 10
 			similar_movies = []
-			genre = MovieGenre
-			for similar_movie in movie.genre.all():
-				similar_movies.append(
-					{
-						'id'           : similar_movie.id,
-						'korean_title' : similar_movie.movie_set.korean_title,
-						'thumbnail_img': similar_movie.movie_set.thumbnail_img,
-						'netflix'      : similar_movie.movie_set.netflix,
-						'watcha'       : similar_movie.movie_set.watcha,
-					}
-				)
+#			for similar_movie in movie.genre:
+#				similar_movies.append(
+#					{
+#						'id'           : similar_movie.id,
+#						'korean_title' : similar_movie.korean_title,
+#						'thumbnail_img': similar_movie.thumbnail_img,
+#						'netflix'      : similar_movie.netflix,
+#						'watcha'       : similar_movie.watcha,
+#					}
+#				)
 	
 			movie_information = {
                                 'id'            : movie.id,
@@ -47,31 +46,9 @@ class MovieInformationView(View):
                                 'discription'   : movie.discription,
                                 'thumbnail_img' : movie.thumbnail_img,
                                 'background_img': movie.background_img,
-                                'genre'         : movie.genre.name,
+                                'genre'         : 
 				'comments'      : movie_comments,
 				'similar_movies': similar_movies,
 				}
 
-		return JsonResponse({'movie_information': movie_information}, status = 200)
-	
-#	def post(self, request, comment_id):
-#		data = json.loads(request.body)
-#	#	try:
-#			movie_check = Movie.objects.filter(id=data["movieId"])
-#			
-#            	# 영화가 없을 경우
-#			if not movie_check.exists():
-#				return JsonResponse({"message": "NO_MOVIE"}, status=40)
-#			
-#			comment_check = Comment.objects.filter(
-#                	user_id=request.user.id,
-#                	movie_id=data["movieId"]
-#            		)
-#	def patch(self, request, comment_id)
-
-#	def delete(self, request, comment_id)
-
-# 상세페이지 정보 주기(댓글 좋아요 순으로 뿌려주기, 영화 관객순으로 뿌려주기)
-# 댓글기능(추가,수정,삭제)
-# 좋아요 누르기 기능
-# 장바구니 추가 기능
+		return JsonResponse({'movie_information': movie_information}, status = 200)	
