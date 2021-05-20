3e14792089877c7b68b61518c61c96787e3e7536
최대환 <daehwan@choedaehwan-ui-MacBookPro.local>
2021-05-20 17:56:17 +0900

    q객체를 사용해서 영화 정보 주는 기능 수정

--- movies/views.py
+++ movies/views.py
@@ -13,36 +13,24 @@ class MovieInfoView(View):
 		provider        = request.GET.get('provider')
 		results = []
 		q = Q()
+
+		LIMIT   = 10
+		orderby = '?'
 		if ranking == 'box-office':
-			q = ranking
-			LIMIT = 25
-			orderyby = movie_ranking_data
+			q       = Q()
+			data    = 'movie_ranking_data'
+			LIMIT   = 25
+			orderby = '-audience_count'
 
 		if provider == 'netflix':
-			q = Q(netflix = True)
-			LIMIT = 10
-			orderby = ?
+			q       = Q(netflix = True)
+			data    = 'netflix_random_data'
 
 		if provider == 'watcha':
-			q = Q(watcha = True)
-			LIMIT = 10
+			q       = Q(watcha = True)
+			data    = 'watcha_random_data'
 
-
-			movie_ranking_data = Movie.objects.values(
-						'id',
-                                                'korean_title',
-						'english_title',
-                                                'country',
-                                                'release_date',
-                                                'thumbnail_img',
-						'netflix',
-						'watcha',
-                                                ).order_by('-audience_count')[:LIMIT]
-			results.append(list(movie_ranking_data))
-		
-		if provider == 'netflix':
-			LIMIT = 10
-			netflix_random_data = Movie.objects.filter(Q(netflix=True)).values(
+		data  = Movie.objects.filter(q).values(
 						'id',
 						'korean_title', 
 						'english_title',
@@ -51,22 +39,8 @@ class MovieInfoView(View):
 						'thumbnail_img',
 						'netflix',
 						'watcha',
-						).order_by('?')[:LIMIT]
-			results.append(list(netflix_random_data))
-
-		if provider == 'watcha':
-			LIMIT = 10
-			watcha_random_data = Movie.objects.filter(Q(watcha=True)).values(
-						'id',
-						'korean_title',
-						'english_title',
-                                                'country',
-                                                'release_date',
-                                                'thumbnail_img',
-						'netflix',
-						'watcha',
-                                                )[:LIMIT]
-			results.append(list(watcha_random_data))
+					).order_by(orderby)[:LIMIT]
+		results.append(list(data))
 
 		return JsonResponse({'MESSAGE': results}, status = 200)
 
