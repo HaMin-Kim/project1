318d47080c4530438a5ba91201cd44af5aea05b0
최대환 <daehwan@choedaehwan-ui-MacBookPro.local>
2021-05-20 17:07:09 +0900

    in progress

--- movies/views.py
+++ movies/views.py
@@ -4,12 +4,31 @@ from django.views     import View
 from django.http      import JsonResponse
 
 from users.utils      import login_confirm
-from movies.models    import Movie, Comment, Like, Genre, MovieGenre
+from movies.models    import Movie, Comment, Like, Genre, MovieGenre, RatingMovie
+
 
 class MovieInfoView(View):
+	@ login_confirm2
 	def get(self, request, movie_id):
 		if Movie.objects.filter(id = movie_id).exists():
-			movie    = Movie.objects.get(id = movie_id)
+			movie = Movie.objects.get(id = movie_id)
+			user  = request.user
+		
+			# 보고싶어요
+			wish_movie = WishMovie.objects.filter(user = user, movie = movie.id)
+			if wish_movie.exists():
+				wish_check = 1
+
+			elif not wish_movie.exists():
+                        	wish_check = 0
+
+			# 별점 
+			rating_movie = RatingMovie.objects.filter(user = user, movie = movie.id)
+			if rating_movie.exists():
+				star_check = rating_movie.rating
+
+			elif rating_movie.exists():
+				star_check = 0
 
 			movie_comments = [
 				{
@@ -36,6 +55,7 @@ class MovieInfoView(View):
                                 'release_date'  : movie.release_date,
                                 'running_time'  : movie.running_time,
                                 'discription'   : movie.discription,
+				'wish_check'    : wish_check,
                                 'thumbnail_img' : movie.thumbnail_img,
                                 'background_img': movie.background_img, 
 				'genre'         : list(movie.genre.values('name')),
