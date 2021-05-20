69c840c3c350a4bf1513e47e529cde2a1c19ebd7
김하민 <khmin1017@gmail.com>
2021-05-20 17:36:22 +0900

    Add : 상세페이지 별점 post 기능 구현

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls   import path
 
-from movies.views  import MovieInfoView, MovieCommentView, CommentLikeView
+from movies.views  import MovieInfoView, MovieCommentView, CommentLikeView, Rating
 
 urlpatterns = [
 	path('/<int:movie_id>', MovieInfoView.as_view()),
 	path('/<int:movie_id>/comment', MovieCommentView.as_view()),
 	path('/<int:movie_id>/comment/<int:comment_id>', MovieCommentView.as_view()),
 	path('/comment/like', CommentLikeView.as_view()),
+	path('/<int:movie_id>/rating', Rating.as_view()),
 ]
